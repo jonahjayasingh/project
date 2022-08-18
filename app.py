565336c5ef2fd7ca1c1168b7ed4d3c831f7d4a5b
jonah jayasingh <81973624+jonahjayasingh@users.noreply.github.com>
2022-08-18 09:08:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,119 @@
+from flask import Flask,render_template,request
+from flask_mysqldb import MySQL
+import datetime
+
+
+app = Flask(__name__)
+
+# Configure db
+
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "jonah"
+
+mysql = MySQL(app)
+
+
+
+@app.route("/")
+
+def home():
+    return render_template("home.html")
+
+@app.route("/Registerdonor", methods=["POST","GET"])
+
+def Registerdonor():
+    if request.method == 'POST':
+        doby =  request.form.get('date').split("-")[0]
+        year = datetime.datetime.now().year
+        age  =  year - int(doby)
+        if age > 18:
+            try:
+                fname = request.form.get('fname')
+                sname = request.form.get('sname')
+                password = request.form.get('password')
+                email = request.form.get('email')
+                date =  request.form.get('date')
+                gender = request.form.get('gender')
+                
+                cur = mysql.connection.cursor()
+                cur.execute("INSERT INTO pd     (fname,sname,password,email,date,gender) VALUES ('{}','{}','{}','{}','{}','{}')".format(fname,sname,password,email,date,gender))
+                mysql.connection.commit()
+                cur.close()
+                return render_template("home.html")
+            except:
+                return render_template("registerdonor.html", message= "Mail ID is Alredy Registered try Logging in")
+                    
+        else:
+            return render_template("home.html", message= "sorry you cann't donate plasma")
+    return render_template("registerdonor.html")
+
+@app.route("/RR",methods=["POST","GET"])
+
+def RR():
+    if request.method == "POST":
+        try:
+                fname = request.form.get('fname')
+                sname = request.form.get('sname')
+                password = request.form.get('password')
+                email = request.form.get('email')
+                gender = request.form.get('gender')
+                hospital_name = request.form.get('hospital_name')
+                
+                cur = mysql.connection.cursor()
+                cur.execute("INSERT INTO pr(fname,sname,password,email,gender,hospital_name) VALUES ('{}','{}','{}','{}','{}','{}')".format(fname,sname,password,email,gender,hospital_name))
+                mysql.connection.commit()
+                cur.close()
+                return render_template("home.html")
+        except:
+                return render_template("rr.html", message= "Mail ID is Alredy Registered try Logging in")
+    return render_template("rr.html")
+
+
+@app.route("/LR",methods=["POST","GET"])
+
+def LR():
+    message = ""
+    if request.method == "POST" and "email" in request.form and "password" in request.form:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        cur = mysql.connection.cursor()
+        cur.execute("select * from pr where email = '{}' and password = '{}';".format(email,password))
+        user = cur.fetchone()
+        print(user)
+        if user:
+            email = user[2]
+            return render_template("home.html", email = email)
+        else:
+            message = "please type correct email or password "
+            return render_template("lr.html", message=message)
+    return render_template("lr.html")
+
+
+
+@app.route("/Logindonor", methods=["POST","GET"])
+
+def Logindonor():
+    message = ""
+    if request.method == "POST" and "email" in request.form and "password" in request.form:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        cur = mysql.connection.cursor()
+        cur.execute("select * from pd where email = '{}' and password = '{}';".format(email,password))
+        user = cur.fetchone()
+        if user:
+            email = user[2]
+            return render_template("home.html", email = email)
+        else:
+            message = "please type correct email or password "
+            return render_template("logindonor.html", message=message)
+    return render_template("logindonor.html", message=message)
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
---
+++ jonah.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 17, 2022 at 08:14 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jonah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pd`
+--
+
+CREATE TABLE `pd` (
+  `fname` char(30) DEFAULT NULL,
+  `sname` char(30) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pd`
+--
+
+INSERT INTO `pd` (`fname`, `sname`, `email`, `password`, `date`, `gender`) VALUES
+('Jonah', 'Jayasingh', 'jayasinghjonah@gmail.com', 'jonah', '2002-07-11', 'M');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pr`
+--
+
+CREATE TABLE `pr` (
+  `fname` char(30) DEFAULT NULL,
+  `sname` char(30) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `hospital_name` varchar(100) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pr`
+--
+
+INSERT INTO `pr` (`fname`, `sname`, `email`, `hospital_name`, `password`, `gender`) VALUES
+('Jonah', 'Jonah', 'jayasinghjonah@gmail.com', 'Jonah', 'jonah', 'o'),
+('king', 'King', 'King@gmail.com', 'king', 'king', 'o');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pd`
+--
+ALTER TABLE `pd`
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `pr`
+--
+ALTER TABLE `pr`
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `hospital_name` (`hospital_name`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

57991a5dfbf4650d119f6d3ed0052268f67a4d9e
jonah jayasingh <jayasinghjonah24@gmail.com>
2022-08-22 22:08:03 +0530

    sql updated

--- jonah.sql
+++ jonah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2022 at 08:14 PM
+-- Generation Time: Aug 22, 2022 at 06:33 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `jonah`
 --
+CREATE DATABASE IF NOT EXISTS `jonah` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `jonah`;
 
 -- --------------------------------------------------------
 
@@ -33,13 +35,19 @@ CREATE TABLE `pd` (
   `email` varchar(40) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `date` date DEFAULT NULL,
-  `gender` char(1) DEFAULT NULL
+  `gender` char(1) DEFAULT NULL,
+  `Blood_group` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `pd`
 --
 
+INSERT INTO `pd` (`fname`, `sname`, `email`, `password`, `date`, `gender`, `Blood_group`) VALUES
+('Jonah', 'Jayasingh', 'jayasinghjonah@gmail.com', 'jonah', '2002-07-11', 'M', 'B+'),
+('ghost', 'ghost', 'ghost@gmail.com', 'ghost', '2002-07-11', 'M', 'O-'),
+('king', 'king', 'king@gmail.com', 'king', '2000-07-25', 'M', 'O-'),
+('tom', 'tom', 'tom@gmail.com', 'tom', '2000-05-25', 'M', 'A+');
 
 -- --------------------------------------------------------
 
@@ -60,6 +68,9 @@ CREATE TABLE `pr` (
 -- Dumping data for table `pr`
 --
 
+INSERT INTO `pr` (`fname`, `sname`, `email`, `hospital_name`, `password`, `gender`) VALUES
+('king', 'King', 'King@gmail.com', 'king', 'king', 'M'),
+('ghost', 'ghost', 'ghost@gmail.com', 'ghost', 'ghost', 'M');
 
 --
 -- Indexes for dumped tables
@@ -77,7 +88,6 @@ ALTER TABLE `pd`
 ALTER TABLE `pr`
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `hospital_name` (`hospital_name`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
